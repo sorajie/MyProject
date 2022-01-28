2176b67d3347ac5fed68ce1b25e7d721a77d1ba6
sorajie <295249318@qq.com>
2022-01-28 14:06:32 +0800

    first commit

---
+++ src/test1.java
@@ -0,0 +1,10 @@
+public class test1 {
+    public static void main(String[] arg){//psvm
+        for(int i=0;i<4;i++){
+            for(int j=0;j<9;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    }
